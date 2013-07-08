dca003a5b4860b5517d123a94455b5b2d335376f
Joseph Keshet <katysei@gmail.com>
2013-07-08 18:03:36 +0000

    index.html

--- web.js
+++ web.js
@@ -3,8 +3,13 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!2');
+  
+    var fs=require('fs');
+    var str =fs.readFileSync("index.html", "utf-8");
+    
+    response.send(str);
 });
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
